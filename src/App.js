aca606ab0195232903765e580a41bba9b1450506
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-02-09 16:46:35 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import './App.css';
+import TodoList from './TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-      <p>hi</p>
+    <div className="todo-app">
+      <TodoList />
     </div>
-  ); 
+  );
 }
 
 export default App;
